4879d8aed86045eb472acddf58da4b11f970d7e3
dasom <dasom589@gmail.com>
2024-01-13 21:18:38 +0900

    2024-01-13

--- src/com/javaex/ex03/Goods.java
+++ src/com/javaex/ex03/Goods.java
@@ -5,6 +5,30 @@ public class Goods {
 	private String name;
 	private int price;
 	private int count;
+	
+	public Goods() {
+		super();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
 
 	
 
--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -1,8 +1,42 @@
 package com.javaex.ex03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GoodsApp {
 
     public static void main(String[] args) {
+    	
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("상품을 입력해주세요(종료 q)");
+    	
+    	Goods obj01=new Goods();
+    	obj01.setName(sc.next());
+    	obj01.setPrice(sc.nextInt());
+    	obj01.setCount(sc.nextInt());
+    	
+    	Goods obj02=new Goods();
+    	obj02.setName(sc.next());
+    	obj02.setPrice(sc.nextInt());
+    	obj02.setCount(sc.nextInt());
+    	
+    	Goods obj03=new Goods();
+    	obj03.setName(sc.next());
+    	obj03.setPrice(sc.nextInt());
+    	obj03.setCount(sc.nextInt());
+    	
+    	List<Goods> gList=new ArrayList<Goods>();
+    	gList.add(obj01);
+    	gList.add(obj02);
+    	gList.add(obj03);
+    	
+    	if (sc.next().equals("q")) {
+    		sc.close();
+    		System.out.println("[입력완료]");
+    		System.out.println("=============================");
+    		gList.size();
+    	}
 
        
     }
